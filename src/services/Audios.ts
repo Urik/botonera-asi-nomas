2fe51c3fe4c9ec7205c12522fc4f9aebb5c9768b
Uri Berman <uriberman@gmail.com>
2019-01-24 00:41:55 -0600

    Normalized audios

--- src/services/Audios.ts
+++ src/services/Audios.ts
@@ -22,11 +22,13 @@ export const Audios = [
 ];
 
 export const Music = [
-    media('ryu.jpg', 'ryu_music.mp3', ''),
-    media('goku1.jpg', 'dbz_fight1.mp3', ''),
     media('psycho_killer.jpg', 'psycho_killer.mp3'),
     media('ultraviolento.jpg', 'ultraviolento.mp3'),
-    media('soraya1.jpg', 'soraya.mp3'),
     media('alf.jpg', 'alf.mp3'),
-    media('goku2.jpg', 'dbz_fight2.mp3')
+    media('goku1.jpg', 'dbz_fight1.mp3', ''),
+    media('goku2.jpg', 'dbz_fight2.mp3'),
+    media('heman.jpg', 'heman.mp3'),
+    media('ryu.jpg', 'ryu_music.mp3', ''),
+    media('soraya1.jpg', 'soraya.mp3'),
+    media('bycicle.jpg', 'villanos.mp3')
 ]
